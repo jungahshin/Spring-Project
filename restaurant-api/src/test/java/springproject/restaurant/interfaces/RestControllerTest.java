52be678d18e0089b0cdc63c02bad7ef4e1b41e67
jungahshin <shinasong@naver.com>
2020-08-02 23:56:55 +0900

    [ADD] RestaurantService의 MockBean

--- restaurant-api/src/test/java/springproject/restaurant/interfaces/RestControllerTest.java
+++ restaurant-api/src/test/java/springproject/restaurant/interfaces/RestControllerTest.java
@@ -4,16 +4,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import springproject.restaurant.application.RestaurantService;
-import springproject.restaurant.domain.MenuItemRepository;
-import springproject.restaurant.domain.MenuItemRepositoryImpl;
-import springproject.restaurant.domain.RestRepository;
-import springproject.restaurant.domain.RestRepositoryImpl;
+import springproject.restaurant.domain.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.core.StringContains.containsString;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -26,28 +28,38 @@ public class RestControllerTest {
     @Autowired
     private MockMvc mvc;
 
-    // RestaurantService를 만들어주었기 때문에 RestRepository와 MenuItemRepository가 없어도 된다.
-    @SpyBean(RestaurantService.class)
+    //RestaurantService의 가짜객체를 만들어주었기 때문에 -> 원래 있었던 SpyBean(MenuItemRepositoryImpl, RestRepositoryImpl)가 팔요 없다.
+    @MockBean
     private RestaurantService restaurantService;
 
-    // SpyBeam : 의존성 주입!(Spring의 특징) -> 사용해야 하는 객체를 다양하게 변경할 수 있다.
-    // 기존에 controller는 직접적으로 repository에 의존을 하고 있었는데 이러한 의존성을 분리해주었다.
-    // 직접 구현체인 'RestRepositoryImpl'를 만들고 그 안에 있던 메소드를 interface인 'RestRepository'로 분리해주었다.
-    @SpyBean(RestRepositoryImpl.class)
-    private RestRepository restaurantRepository;
-
-    @SpyBean(MenuItemRepositoryImpl.class)
-    private MenuItemRepository menuItemRepository;
+//    // RestaurantService를 만들어주었기 때문에 RestRepository와 MenuItemRepository가 없어도 된다.
+//    @SpyBean(RestaurantService.class)
+//    private RestaurantService restaurantService;
+//
+//    // SpyBeam : 의존성 주입!(Spring의 특징) -> 사용해야 하는 객체를 다양하게 변경할 수 있다.
+//    // 기존에 controller는 직접적으로 repository에 의존을 하고 있었는데 이러한 의존성을 분리해주었다.
+//    // 직접 구현체인 'RestRepositoryImpl'를 만들고 그 안에 있던 메소드를 interface인 'RestRepository'로 분리해주었다.
+//    @SpyBean(RestRepositoryImpl.class)
+//    private RestRepository restaurantRepository;
+//
+//    @SpyBean(MenuItemRepositoryImpl.class)
+//    private MenuItemRepository menuItemRepository;
 
     @Test
     public void list() throws Exception {
+        //따라서, controller는 restaurantService를 활용한다는 자체에 집중할 뿐이지, restaurantService가 어떻게 작동하는 지는 controller의 관심사가 아니다!
+        List<Rest> restaurants = new ArrayList<>();
+        restaurants.add(new Rest(1004L, "Joker", "Seoul"));
+
+        given(restaurantService.getRestaurants()).willReturn(restaurants);
+
         mvc.perform(get("/restaurants"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(
                         containsString("\"id\":1004")
                 ))
                 .andExpect(content().string(
-                        containsString("\"name\":\"Bob zip\"")
+                        containsString("\"name\":\"Joker\"")
                 ));
 
     }
